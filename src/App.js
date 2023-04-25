4eca43c63d41b6fde828c01cd56f7d76529c149a
Andriikryl <andreyk557@gmail.com>
2023-04-25 17:01:28 +0300

    git-init

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import "./App.css";
+import Search from "./components/search/search";
+
+function App() {
+  return (
+    <div className="App">
+      <div className="container">
+        <Search />
+      </div>
+    </div>
+  );
+}
+
+export default App;
